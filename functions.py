20c5ddefd493c0238d1d4143c2e23916efb77983
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-02 20:18:45 -0500

    addedan icon

--- functions.py
+++ functions.py
@@ -1,82 +1,101 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename 
+import json
+ 
 
-class app(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+LARGEFONT =("Verdana", 25)
+ 
+class MarkDownNotes(tk.Tk):
+    
+    # __init__ function for class MarkDownNotes 
+    def __init__(self, *args, **kwargs): 
         
-        # Container for Pages
-        container = tk.Frame(self)
-        container.pack(side = "top", fill = "both", expand= True)
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
         
+        # Set the window icon - must be done AFTER tk.Tk.__init__
+        try:
+            icon = tk.PhotoImage(file='Markdown_notes_icon_256.png')
+            self.iconphoto(False, icon)
+        except:
+            print("Icon file not found or invalid format")
+        
+        # creating a container
+        container = tk.Frame(self)  
+        container.pack(side = "top", fill = "both", expand = True) 
+ 
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
-        
-        # put the frames in an empty array
-        self.frames = {}
-        
-        # Editing frames through a truple 
-        # With different pages
-        
-        for F in (edit_md, note_view):
-            
+ 
+        # initializing frames to an empty array
+        self.frames = {}  
+ 
+        # Making frames through a truple
+        for F in (edit_md, view_notes):
+ 
             frame = F(container, self)
-            
-            # make a frame of the object from 
-            # edit page and notes view wit a for loop
-            self.frames[F] = frame
-            
-            frame.grid(row=0, column = 0, sticky = "nsew")
-        
-        self.show_frame(edit_md)
-        
-        # display the current frame as a parameter
-        def show_frame(self, cont):
-            frame = self.frame[cont]
-            frame.tkraise()
-            
-# First window frame
-class edit_md(tk.Frame):
-    def __init__(self, parent, controller):
+ 
+            # Making frames through a loop
+            self.frames[F] = frame 
+ 
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+ 
+        self.show_frame(view_notes)
+ 
+    # display the frame through a parameter change
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class view_notes(tk.Frame):
+    def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
         
-        # Label of frame layout 2
-        label = ttk.Label(self, text = "edit_md")
+        # label of frame Layout 2
+        label = ttk.Label(self, text ="Notes", font = LARGEFONT)
         
-        # making a grid
-        label.grid(row = 0, column = 4, padx= 10, pady = 10)
-        
-        button1 = ttk.Button(self, text = "edit", command = lambda : controller.show_frame(note_view))
-        
-        # placing the button
-        button1.grid(row = 1, column = 1, padx=10, pady=10)
-        
-        # button on frame 2 (notes view)
-        button2 = ttk.Button(self, text = "Notes", command = lambda : controller.show_frame(edit_md))
-        
-        # placing the button
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
-        
-class note_view(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Notes", command= lambda : controller.show_frame(edit_md)) 
-        
-        # Button to show edit_md page
-        button1 = ttk.Button(self, text="Edit", command = lambda : controller.show_frame(edit_md))
-        
-        # placing the button
+        # putting the grid in its place by using
+        # grid
+        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+ 
+        button1 = ttk.Button(self, text ="View Notes",
+        command = lambda : controller.show_frame(  view_notes))
+    
+        # putting the button in its place by
+        # using grid
         button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+ 
+        ## button to show frame 2 with text layout2
+        button2 = ttk.Button(self, text ="Open note",command = lambda : controller.show_frame(edit_md))
+    
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+ 
+         
+ 
+ 
+# second window frame page1 
+class edit_md(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
         
-        # Button  to show the notes(in the notes frame)
-        button2 = ttk.Button(self, text = "Notes", command = lambda : controller.show_frame(note_view))
-        button2.grid(row=2, column=1, padx = 10, pady = 10)
+        label = ttk.Label(self, text ="Edit Note", font = LARGEFONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+ 
+        View_notes = ttk.Button(self, text ="View Notes",command = lambda : controller.show_frame(edit_md))
+        # placing the button
+        Edit_md = ttk.Button(self, text ="Open Note",command = lambda : controller.show_frame(view_notes))
+    
+        # placing the buttons
+        Edit_md.grid(row = 1, column = 0, padx = 10, pady = 10)
+        View_notes.grid(row = 2, column = 0, padx = 10, pady = 10)
+ 
         
         
         
-        
-#Making the window
-app = app()
-app.mainloop()       
-        
+# Driver Code
+app = MarkDownNotes()
+app.mainloop()
